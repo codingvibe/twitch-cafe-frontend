073b8454d2f6606cedc268583605fd8d7e0c4b8e
Colby <colby.sites@gmail.com>
2022-08-10 00:27:21 -0500

    Updating everything and switching to vite build.

--- svelte.config.js
+++ svelte.config.js
@@ -9,11 +9,7 @@ const config = {
 		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
-		},
-		files: {
-            assets: 'static',
-            lib: 'src/lib'
-        }
+		}
 	},
 	preprocess: sveltePreprocess()
 };
